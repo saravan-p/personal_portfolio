f0135e585be4b449599af7cbf394217e04eb0aa1
saravan-p <43927034+saravan-p@users.noreply.github.com>
2025-07-17 16:42:24 -0400

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -103,7 +103,8 @@ const Experience = () => {
               onClick={() =>
                 window.open(
                   //'https://github.com/saravan-p/resume/blob/main/saravan-Resume%20.pdf'
-                  'https://github.com/saravan-p/resume/blob/a7482da65d4ea006cd769b66975f8330bda55ca2/Resume.pdf', //paste the link to your resume here
+                  //'https://github.com/saravan-p/resume/blob/a7482da65d4ea006cd769b66975f8330bda55ca2/Resume.pdf'
+                  'https://github.com/saravan-p/resume/blob/4fcde423261f5ac3471e9cca816f92d490ce0828/saravan.pdf', //paste the link to your resume here
                   '_blank'
                 )
               }
